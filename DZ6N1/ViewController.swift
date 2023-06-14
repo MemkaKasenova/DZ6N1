b001779c9ebb9d4b696dffa9b1cb2f0f4d815e76
Memka <mkasenova87@gmail.com>
2023-06-14 11:45:57 +0600

    Initial Commit

---
+++ DZ6N1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DZ6N1
+//
+//  Created by merim kasenova on 14/6/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
